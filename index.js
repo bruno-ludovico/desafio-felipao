f2e70dc095ed7d826293d8ea5d118376ea223f09
bruno-ludovico <bruno_ludovico@hotmail.com>
2025-04-24 16:35:40 -0300

    desafio-felipao

---
+++ index.js
@@ -0,0 +1,10 @@
+let nomeHeroi=""
+let xpHeroi=0
+Switch (xpHeroi){
+    case <=1000:
+        console.log("O herói "+nomeHeroi+" está no nível Ferro")
+        break
+    case >1001 && <=2000:
+        console.log("O herói "+nomeHeroi+" está no nível Bronze")
+        break
+}
