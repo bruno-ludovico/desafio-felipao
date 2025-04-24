243a76ff93bfaf6323f807529bcda9880bb13da5
bruno-ludovico <bruno_ludovico@hotmail.com>
2025-04-24 17:22:06 -0300

    desafio-final

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let nomeHeroi=""
-let xpHeroi=0
-Switch (xpHeroi){
-    case <=1000:
-        console.log("O herói "+nomeHeroi+" está no nível Ferro")
-        break
-    case >1001 && <=2000:
-        console.log("O herói "+nomeHeroi+" está no nível Bronze")
-        break
+let nomeHeroi="Felipão"
+let xpHeroi=10000
+
+if(xpHeroi <=1000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ferro")
+} else if (xpHeroi > 1000 && xpHeroi<= 2000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Bronze")
+} else if (xpHeroi > 2000 && xpHeroi<= 5000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Prata")
+} else if (xpHeroi > 5000 && xpHeroi<= 7000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ouro")
+} else if (xpHeroi > 7000 && xpHeroi<= 8000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Platina")
+} else if (xpHeroi > 8000 && xpHeroi<= 9000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Ascendente")
+} else if (xpHeroi > 9000 && xpHeroi<= 10000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Imortal")
+} else if (xpHeroi >=1000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível Radiante")
 }
